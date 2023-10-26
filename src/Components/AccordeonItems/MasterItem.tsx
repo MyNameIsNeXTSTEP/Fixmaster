18770c040c5015ee24be2bd3dd6a271539d1d161
Samur <107584285+Samur000@users.noreply.github.com>
2023-10-26 18:36:45 +0300

    [#862kg89t6] разработка карточки салона в детальной странице

---
+++ src/Components/AccordeonItems/MasterItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const MasterItem = () => {
+  return (
+    <div >
+
+    </div >
+  );
+};
+
+export default MasterItem;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
-import App from './app'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById('app-root')!;
-const root = createRoot(container);
-root.render(<App/>);
+import salonsData from "./salonsData";
+
+const container = document.getElementById( "app-root" )!;
+const root = createRoot( container );
+
+
+
+
+root.render(
+  <React.StrictMode >
+    <BrowserRouter >
+      <App />
+    </BrowserRouter >
+  </React.StrictMode >,
+);
