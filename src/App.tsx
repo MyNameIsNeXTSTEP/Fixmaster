d0ac4f2c4e530cd4f702d8d79c93b8aba36d772f
Islam Gadzhiev <islamgadzhiev@Islams-MacBook-Pro.local>
2023-10-26 21:55:16 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React from "react";
-import SaloonCard from "./Components/SaloonCard";
-import { Link, Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/Main";
 import SalonPage from "./pages/SalonPage";
 
 const App = (): JSX.Element => {
-  return <>
-        <h1 >The Fixmaster app</h1 >
-        <Link to="/" >Главная </Link >
-      <ul >
-        <Link to="/salon/1" >Салон 1</Link ><br />
-        <Link to="/salon/2" >Салон 2</Link ><br />
-        <Link to="/salon/3" >Салон 3</Link ><br />
-        <Link to="/salon/4" >Салон 4</Link ><br />
-        <Link to="/salon/5" >Салон 5</Link ><br />
-      </ul >
-
-
-        <Routes >
-            <Route path="/" element={ <MainPage /> } />
-            <Route path="/salon/:id" element={ <SalonPage /> } />
-        </Routes >
-    </>;
+  return (
+    <Routes>
+        <Route path="/" element={ <MainPage/> }/>
+        <Route path="/salon/:id" element={ <SalonPage/> }/>
+    </Routes>
+    );
 };
 
 export default App;
