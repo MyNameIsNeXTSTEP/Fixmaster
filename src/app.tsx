7add48b2f4ca963ce74279f00ba1f723c711ecb3
Samur <107584285+Samur000@users.noreply.github.com>
2023-11-15 22:08:02 +0300

    rebase_4

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import MainPage from "./pages/Main";
-import SalonPage from "./pages/SalonPage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/Main';
+import SalonPage from './pages/SalonPage';
+import RecordPage from './pages/RecordPage';
 
 const App = (): JSX.Element => {
-  return (
-    <Routes>
-        <Route path="/" element={ <MainPage/> }/>
-        <Route path="/salon/:id" element={ <SalonPage/> }/>
-    </Routes>
-    );
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={ <MainPage/> }/>
+				<Route path='/salon/:id' element={ <SalonPage/> }/>
+				<Route path='/record/:id' element={ <RecordPage/> }/>
+			</Routes>
+		</>
+	);
 };
 
 export default App;
