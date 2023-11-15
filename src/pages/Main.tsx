ad76b25fed6a9ab49bf1c209d990b5d266305bc2
Samur <107584285+Samur000@users.noreply.github.com>
2023-11-15 22:05:26 +0300

    rebase_2

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,8 +3,17 @@ import { Link } from "react-router-dom";
 
 const MainPage = (): JSX.Element => {
   return <>
+    <h1 >The Fixmaster app</h1 >
     <h2 >MainPage</h2 >
-    </>;
+    <Link to="/" >Главная </Link >
+    <ul >
+        <Link to="/salon/1" >Салон 1</Link><br/>
+        <Link to="/salon/2" >Салон 2</Link><br/>
+        <Link to="/salon/3" >Салон 3</Link><br/>
+        <Link to="/salon/4" >Салон 4</Link><br/>
+        <Link to="/salon/5" >Салон 5</Link><br/>
+    </ul >
+    </>
 };
 
 export default MainPage;
