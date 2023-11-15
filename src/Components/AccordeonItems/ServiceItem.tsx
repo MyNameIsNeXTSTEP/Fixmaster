59275ef6ff379a5ddbe0ca2914234562511dc78c
Samur <107584285+Samur000@users.noreply.github.com>
2023-11-15 22:07:40 +0300

    rebase_3

--- src/Components/AccordeonItems/ServiceItem.tsx
+++ src/Components/AccordeonItems/ServiceItem.tsx
@@ -1,55 +1,58 @@
-import React from "react";
-import styled from "styled-components";
-import { Card } from "react-bootstrap";
+import React from 'react';
+import styled from 'styled-components';
+import { Card } from 'react-bootstrap';
 
-export const C= styled.div`
-margin-bottom: 10px;
-`
+const ServiceCard = styled.div`
+  cursor: pointer;
+
+  .card-title {
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  .price {
+    font-size: 18px;
+    font-weight: 600;
+    white-space: nowrap;
+  }
+`;
+export const MyContainer = styled.div`
+  margin: 10px 0;
+`;
 
 Card.Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 3px 15px;
-  border-bottom: 1px solid #bbbbbb;
-  background-color: #e6eeff;
-`;
-const CardTitle = styled.div`
-  font-size: 20px;
-  font-weight: 600;
-`
-const Price = styled.div`
-  font-size: 18px;
-  font-weight: 600;
-  white-space: nowrap;
+  padding: 0px 15px;
+  //border-bottom: 1px solid #bbbbbb;
 `;
 const Min = styled.div`
   font-size: 12px;
 `;
 
-const ServiceItem = ( { text = "", price = 0, minTime = 0, description = "" } ) => {
-  return (
-    <C>
-      <Card >
-        <Card.Header >
-          <CardTitle >{ text }</CardTitle >
-          <div >
-            <Price >
-            от { price } р <br />
-            </Price >
-            <Min>
-              от { minTime } мин
-            </Min>
-          </div >
-        </Card.Header >
-        <Card.Body >
-          <Card.Text >
-            { description }
-          </Card.Text >
-        </Card.Body >
-      </Card >
-    </C >
-  );
+const ServiceItem = ( { text = '', price = 0, minTime = 0, description = '' } ) => {
+	return (
+		<MyContainer>
+            <ServiceCard>
+                <Card>
+                    <Card.Header>
+                        <div className='card-title'>{ text }</div>
+                        <div>
+                            <div className='price'>
+                                от { price } р <br/>
+                            </div>
+                            <div className={ 'min' }>
+                                от { minTime } мин
+                            </div>
+                        </div>
+                    </Card.Header>{ description ? <Card.Body>
+                        <Card.Text>{ description }</Card.Text>
+                    </Card.Body>: '' }
+                </Card>
+            </ServiceCard>
+        </MyContainer>
+	);
 };
 
 export default ServiceItem;
