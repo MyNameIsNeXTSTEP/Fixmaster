f1cafbc2cf541f76fe0c6bf1a44f6bac376c7fcb
Islam Gadzhiev <islamgadzhiev@Islams-MacBook-Pro.local>
2023-11-16 18:58:11 +0300

    All conflicts resolved

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 
 const MainPage = (): JSX.Element => {
-    return <>
-        <Container>
+	return (
         <ul>
             <Link to='/salon/1'>Салон 1</Link><br/>
             <Link to='/salon/2'>Салон 2</Link><br/>
@@ -12,8 +10,7 @@ const MainPage = (): JSX.Element => {
             <Link to='/salon/4'>Салон 4</Link><br/>
             <Link to='/salon/5'>Салон 5</Link><br/>
         </ul>
-        </Container>
-    </>;
+    );
 };
 
 export default MainPage;
