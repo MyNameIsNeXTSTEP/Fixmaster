cf057941c5272c1ca45fefd75f573ef9f9c31ec9
Samur <107584285+Samur000@users.noreply.github.com>
2023-11-16 17:02:05 +0300

    [862kg89t6] - правки по pl

--- src/pages/RecordPage.tsx
+++ src/pages/RecordPage.tsx
@@ -3,10 +3,6 @@ import { Container } from 'react-bootstrap';
 import { Params, useParams } from 'react-router-dom';
 import 'react-calendar/dist/Calendar.css';
 
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ ValuePiece, ValuePiece ];
-
 const RecordPage = () => {
 	const {id = ''} = useParams<Params>();
 
