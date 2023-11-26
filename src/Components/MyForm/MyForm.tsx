1ab2c901b97fabac6a0f2b3cb6b0cb7e492a3467
Minkailov Minkail <mandz050505@gmail.com>
2023-11-26 17:32:10 +0300

    исправил отступы

--- src/Components/MyForm/MyForm.tsx
+++ src/Components/MyForm/MyForm.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import { Button, Form } from 'react-bootstrap';
 import styled from 'styled-components';
-import * as events from 'events';
 import { Link } from 'react-router-dom';
 
 const MyFormStyle = styled.div`
@@ -24,48 +23,48 @@ const MyFormStyle = styled.div`
 
 const MyForm = () => {
 
-	const [nameError, setNameError] = useState('')
+    const [nameError, setNameError] = useState('')
 
-	const handleButton = () => {
-		console.log( 'po' );
-	};
+    const handleButton = () => {
+        console.log( 'po' );
+    };
 
-	const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-		let result = /^[а-яА-Я ]+$/;
-		if (!result.test(String(e.target.value).toLowerCase()) || e.target.value.length < 2){
-			setNameError('Некорректное имя пользователя')
-		} else setNameError('')
-	}
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let result = /^[а-яА-Я ]+$/;
+        if (!result.test(String(e.target.value).toLowerCase()) || e.target.value.length < 2){
+            setNameError('Некорректное имя пользователя')
+        } else setNameError('')
+    }
 
-	const blurName = (e: React.FocusEvent<HTMLInputElement>) => {
-		if (e.target.value.length === 0) {
-			setNameError('Имя не может быть пустым')
-		}
-	}
+    const blurName = (e: React.FocusEvent<HTMLInputElement>) => {
+        if (e.target.value.length === 0) {
+            setNameError('Имя не может быть пустым')
+        }
+    }
 
-	return (
-		<MyFormStyle>
-			<Form onSubmit={(e) => e.preventDefault()}>
-				<Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-					<Form.Label>Имя</Form.Label>
-					<Form.Control onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleName(e)} onBlur={(e: React.FocusEvent<HTMLInputElement>) => blurName(e)} required type='name' placeholder='Введите ваше имя...'/>
-					{nameError && <div style={{color: 'red'}}>{nameError}</div>}
-				</Form.Group>
-				<Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-					<Form.Label>Телефон</Form.Label>
-					<Form.Control required type='tel' placeholder='Введите ваше номер телефона...'/>
-				</Form.Group>
-				<Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
-					<Form.Label>Комментарий</Form.Label>
-					<Form.Control as='textarea' rows={3}/>
-				</Form.Group>
-				<Form.Group className='mb-3 form-btn-check'>
-					<Form.Check required label='Даю согласие на обработку персональных данных' feedback='You must agree before submitting.' feedbackType='invalid'/>
-					<Button onClick={handleButton} type='submit'><Link to='/recorded'>Записаться</Link></Button>
-				</Form.Group>
-			</Form>
-		</MyFormStyle>
-	);
+    return (
+        <MyFormStyle>
+            <Form onSubmit={(e) => e.preventDefault()}>
+                <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+                    <Form.Label>Имя</Form.Label>
+                    <Form.Control onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleName(e)} onBlur={(e: React.FocusEvent<HTMLInputElement>) => blurName(e)} required type='name' placeholder='Введите ваше имя...'/>
+                    {nameError && <div style={{color: 'red'}}>{nameError}</div>}
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+                    <Form.Label>Телефон</Form.Label>
+                    <Form.Control required type='tel' placeholder='Введите ваше номер телефона...'/>
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+                    <Form.Label>Комментарий</Form.Label>
+                    <Form.Control as='textarea' rows={3}/>
+                </Form.Group>
+                <Form.Group className='mb-3 form-btn-check'>
+                    <Form.Check required label='Даю согласие на обработку персональных данных' feedback='You must agree before submitting.' feedbackType='invalid'/>
+                    <Button onClick={handleButton} type='submit'><Link to='/recorded'>Записаться</Link></Button>
+                </Form.Group>
+            </Form>
+        </MyFormStyle>
+    );
 };
 
 export default MyForm;
